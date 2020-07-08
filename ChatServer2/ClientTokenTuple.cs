2918bb55b1c124ef07aa5ae3dd948e9ff2ac6408
Christopher <slimcognitodudesvids@outlook.com>
2020-07-08 18:49:57 +1000

    Initial commit

---
+++ ChatServer2/ClientTokenTuple.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Sockets;
+using System.Threading;
+
+namespace ChatServer2
+{
+	public class ClientTokenTuple
+	{
+		public TcpClient Client { get; }
+		public CancellationTokenSource TokenSource { get; }
+
+		public ClientTokenTuple(TcpClient client, CancellationTokenSource tokenSource = null)
+		{
+			Client = client;
+			TokenSource = tokenSource ?? new CancellationTokenSource();
+		}
+	}
+}
