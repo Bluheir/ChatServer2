dc4ef3a8ef855379adab06a55b1b3201af3bca52
Christopher <slimcognitodudesvids@outlook.com>
2020-07-13 22:44:43 +1000

    Base message class for all text messages

---
+++ ChatMessages/TextMessage.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace ChatMessages
+{
+	public class TextMessage : BaseMessage
+	{
+		[JsonProperty("contents")]
+		public string Contents { get; set; }
+
+		[JsonProperty("sender")]
+		public int? Sender { get; set; }
+	}
+}
