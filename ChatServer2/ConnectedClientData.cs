5e420716a2432e759d1e5b8ed22ede471941214f
Christopher <slimcognitodudesvids@outlook.com>
2020-07-13 22:45:32 +1000

    Represents data of a tcp client

---
+++ ChatServer2/ConnectedClientData.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatServer2
+{
+	public class ConnectedClientData
+	{
+		public int ClientId { get; set; }
+		public ulong? VoiceClientData { get; set; }
+		public System.Net.IPEndPoint UDPEndpoint { get; set; }
+	}
+}
