37248ff31a76c3b49c4def603bae45755ddc791b
Christopher <slimcognitodudesvids@outlook.com>
2020-07-13 22:43:57 +1000

    Base message class for all messages sent on the tcp server

---
+++ ChatMessages/BaseMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ChatMessages
+{
+	public class BaseMessage
+	{
+		[JsonProperty("message_type")]
+		public string MessageType { get; set; }
+
+		[JsonProperty("value")]
+		public object Value { get; set; }
+	}
+}
